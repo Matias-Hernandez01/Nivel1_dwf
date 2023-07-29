4ede9c7cc593038880f920a17425be44c61b1b63
Matias Hernandez <matias2k00@gmail.com>
2023-07-28 23:54:03 -0300

    consigna a resolver

--- 01-bucles/For/index.js
+++ 01-bucles/For/index.js
@@ -1,5 +1,11 @@
 //? Ejercicio n°1
 
+//*Dado el siguiente array de objetos
+
+//!Usar un for para imprimir en la consola el nombre y la edad de cada animal en el array.
+//!Usar un for in para imprimir en la consola todas las propiedades y valores de cada animal en el array..
+//!Usar un while para imprimir en la consola el tipo de cada animal en el array y detener el loop cuando se encuentra un animal con un tipo de "acuático".
+
 const animales = [
   { nombre: 'perro', tipo: 'mamífero', edad: '4' },
   { nombre: 'gato', tipo: 'mamífero', edad: '2' },
