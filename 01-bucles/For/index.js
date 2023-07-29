4e4f90c0cf286efff40485cd326ea6da10a7d112
Matias Hernandez <matias2k00@gmail.com>
2023-07-28 23:53:00 -0300

    ejercicio consigna

---
+++ 01-bucles/For/index.js
@@ -0,0 +1,9 @@
+//? Ejercicio n°1
+
+const animales = [
+  { nombre: 'perro', tipo: 'mamífero', edad: '4' },
+  { nombre: 'gato', tipo: 'mamífero', edad: '2' },
+  { nombre: 'loro', tipo: 'ave', edad: '6' },
+  { nombre: 'pez', tipo: 'acuático', edad: '1' },
+  { nombre: 'rana', tipo: 'anfibio', edad: '3' },
+];
